8face4adc7e29fd3856b428c0718cccaacbec1fe
Shay Palachy <shaypal5@gmail.com>
2017-03-17 00:43:05 +0200

    trying python setup.py setup again

---
+++ tests/basic_stages/test_val_keep.py
@@ -0,0 +1,23 @@
+"""Testing ValKeep pipline stages."""
+
+import pandas as pd
+
+from pdpipe.basic_stages import ValKeep
+
+
+def test_valdrop_with_columns():
+    """Testing the ColDrop pipline stage."""
+    df = pd.DataFrame([[1, 4], [4, 5], [5, 11]], [1, 2, 3], ['a', 'b'])
+    res_df = ValKeep([4, 5], 'a').apply(df)
+    assert 1 not in res_df.index
+    assert 2 in res_df.index
+    assert 3 in res_df.index
+
+
+def test_valdrop_without_columns():
+    """Testing the ColDrop pipline stage."""
+    df = pd.DataFrame([[1, 4], [4, 5], [5, 11]], [1, 2, 3], ['a', 'b'])
+    res_df = ValKeep([4, 5]).apply(df)
+    assert 1 not in res_df.index
+    assert 2 in res_df.index
+    assert 3 not in res_df.index
