e121a3fbc8b928773113d69955c28b0a1819f411
ShayPalachy <shay@zencity.io>
2018-02-12 19:08:35 +0000

    added nltk extra_requires

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     install_requires=INSTALL_REQUIRES,
     extras_require={
         'sklearn': ['scikit-learn'],
+        'nltk': ['nltk'],
         'test': TEST_REQUIRES
     },
     setup_requires=INSTALL_REQUIRES,
