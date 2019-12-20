9b6225e8ce67761a7f5db212797d16cadf8f82e9
Shay Palachy <shaypal5@gmail.com>
2019-12-20 11:27:02 +0200

    working on auxilary doc and site stuff taken from pdoc3

---
+++ .github/deploy-gh-pages.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -eu
+
+if [ ! -d doc/build ]; then
+    echo 'Error: invalid directory. Deploy from repo root.'
+    exit 1
+fi
+
+[ "$GH_PASSWORD" ] || exit 12
+
+sitemap() {
+    WEBSITE='https://pdpipe.github.io/pdpipe'
+    find -name '*.html' |
+        sed "s,^\.,$WEBSITE," |
+        sed 's/index.html$//' |
+        grep -v '/google.*\.html$' |
+        sort -u  > 'sitemap.txt'
+    echo "Sitemap: $WEBSITE/sitemap.txt" > 'robots.txt'
+}
+
+head=$(git rev-parse HEAD)
+
+git clone -b gh-pages "https://shaypal5:$GH_PASSWORD@github.com/$TRAVIS_REPO_SLUG.git" gh-pages
+mkdir -p gh-pages/doc
+cp -R doc/build/* gh-pages/doc/
+cd gh-pages
+sitemap
+git add *
+git diff --staged --quiet && echo "$0: No changes to commit." && exit 0
+git commit -a -m "CI: Update docs for $TRAVIS_TAG ($head)"
+git push
