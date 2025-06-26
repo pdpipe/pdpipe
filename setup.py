1154f0f8b1a7583c56a630938315a03cb4e0a2f0
Shay Palachy <shaypal5@users.noreply.github.com>
2025-06-26 11:48:58 +0300

    start testing for python 3.12

--- setup.py
+++ setup.py
@@ -68,6 +68,8 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Intended Audience :: Developers",
