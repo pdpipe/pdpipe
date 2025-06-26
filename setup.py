bafaa03bf25a10b3fba8101b880ca0bc646eba92
Shay Palachy-Affek <shaypal5@users.noreply.github.com>
2025-06-26 12:39:30 +0300

    deprecate support for python 3.7 and 3.8 (#117)

--- setup.py
+++ setup.py
@@ -64,8 +64,6 @@ setup(
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
